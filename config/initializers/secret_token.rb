d0d9521ca2578b63baa5393d9f91209722c6076e
yanisf <foris86@mail.ru>
2016-06-01 21:32:30 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BasketballResourceApp::Application.config.secret_key_base = '67c7f3e8cabb288983223d6a09b26b11f13851c2bd5115d61cf919f42d5555929db99a381620838ffa7c81c1991ee0396855ee5bcc438dea0ef1f5adb842296b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BasketballResourceApp::Application.config.session_store :cookie_store, key: '_basketball_resource_app_session'
